b25171688b4a193a7fe271e2f6c6ae29e42ecdc0
ChaEma7 <70863947+ChaEma7@users.noreply.github.com>
2021-09-28 19:10:08 +0200

    28-09-2021
    Index færdig, men mangler stadig count i kategorier

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -13,23 +13,22 @@ let _users = [];
 // =============== Login kode ==================
 // Skrevet af Line
 window.login = () => {
-    const mail = document.querySelector("#login-mail").value;
-    const password =document.querySelector("#login-password").value;
-    console.log(mail, password);
+  const mail = document.querySelector("#login-mail").value;
+  const password = document.querySelector("#login-password").value;
+  console.log(mail, password);
 
-    if(mail === "podcut@mail.com" && password === "podcut"){
-        console.log("Approved");
-        localStorage.setItem("userIsApproved", true);
-        navigateTo("#/");
-    }
-    else {
-        console.log("Not approved");
-        document.querySelector("#notApproved").innerHTML ="Forkert e-mail eller kode. <br> Prøv igen eller opret en bruger";
-    }
-    window.logout = () => {
-        localStorage.setItem("userIsApproved", false);
-        navigateTo("#/login");
-    }
+  if (mail === "podcut@mail.com" && password === "podcut") {
+    console.log("Approved");
+    localStorage.setItem("userIsApproved", true);
+    navigateTo("#/");
+  } else {
+    console.log("Not approved");
+    document.querySelector("#notApproved").innerHTML = "Forkert e-mail eller kode. <br> Prøv igen eller opret en bruger";
+  }
+  window.logout = () => {
+    localStorage.setItem("userIsApproved", false);
+    navigateTo("#/login");
+  }
 }
 
 // ========== OPRET PROFIL ==========
@@ -37,77 +36,79 @@ window.login = () => {
 
 //Fetchs person data from jsonbin
 async function loadUsers() {
- const url = _baseUrl + "/latest"; // make sure to get the latest version
- const response = await fetch(url, { headers: _headers });
- const data = await response.json();
- console.log(data);
- _users = data.record;
- console.log(_users);
- appendUsers(_users);
- 
+  const url = _baseUrl + "/latest"; // make sure to get the latest version
+  const response = await fetch(url, {
+    headers: _headers
+  });
+  const data = await response.json();
+  console.log(data);
+  _users = data.record;
+  console.log(_users);
+  appendUsers(_users);
+
 }
 loadUsers();
 
 function appendUsers() {
-    let htmlTemplate = "";
-    for (let user of _users){
-      htmlTemplate += /*html*/`
+  let htmlTemplate = "";
+  for (let user of _users) {
+    htmlTemplate += /*html*/ `
       <article>
       <h2>${user.name} ${user.surname}</h2>
       <p>${user.mail}</p>
       
       </article>`
-    }
-    document.querySelector("#profile").innerHTML = htmlTemplate;
-    
   }
+  document.querySelector("#profile").innerHTML = htmlTemplate;
+
+}
 
 async function add() {
-    console.log("Add button clicked");
-  
-    let inputName = document.getElementById('inputName');
-    let inputSurName = document.getElementById('inputSurName');
-    let inputMail = document.getElementById('inputMail');
-    let inputPassword = document.getElementById('inputPassword');
-    
-    let newUser = {
-      name: inputName.value,
-      surname: inputSurName.value,
-      mail: inputMail.value,
-      password: inputPassword.value
-      
-    };
-    _users.push(newUser);
-    await updateJSONBIN(_users);
-  
+  console.log("Add button clicked");
+
+  let inputName = document.getElementById('inputName');
+  let inputSurName = document.getElementById('inputSurName');
+  let inputMail = document.getElementById('inputMail');
+  let inputPassword = document.getElementById('inputPassword');
+
+  let newUser = {
+    name: inputName.value,
+    surname: inputSurName.value,
+    mail: inputMail.value,
+    password: inputPassword.value
+
+  };
+  _users.push(newUser);
+  await updateJSONBIN(_users);
+
   //reset af felter
-    inputName.value = "";
-    inputSurName.value = "";
-    inputMail.value = "";
-    inputPassword.value ="";
-    
-  
+  inputName.value = "";
+  inputSurName.value = "";
+  inputMail.value = "";
+  inputPassword.value = "";
+
+
   appendUsers(_users);
-    navigateTo("#/choose-interests");
-  }
+  navigateTo("#/choose-interests");
+}
 
 /**
  * Updates the data source on jsonbin with a given users arrays
  * @param {Array} users 
  */
- async function updateJSONBIN(users) {
-    // put users array to jsonbin
-    const response = await fetch(_baseUrl, {
-      method: "PUT",
-      headers: _headers,
-      body: JSON.stringify(users)
-    });
-  
-    const result = await response.json();
-    console.log(result);
-  
-    appendUsers(result.record);
-  }
+async function updateJSONBIN(users) {
+  // put users array to jsonbin
+  const response = await fetch(_baseUrl, {
+    method: "PUT",
+    headers: _headers,
+    body: JSON.stringify(users)
+  });
+
+  const result = await response.json();
+  console.log(result);
+
+  appendUsers(result.record);
+}
 
 // ======================= Search =====================================
 // skrevet af Chalotte
@@ -116,17 +117,15 @@ function search(value) {
   let searchQuery = value.toLowerCase();
   let filteredPods = [];
   for (let pod of _pods) {
-      let title = pod.title.rendered.toLowerCase();
-      if (title.includes(searchQuery)) {
-          filteredPods.push(pod);
-      }
-      let genre = pod.acf.genre.toLowerCase();
-      if (genre.includes(searchQuery)) {
-        filteredPods.push(pod);
-      }
+    let title = pod.title.rendered.toLowerCase();
+    let genre = pod.acf.genre.toLowerCase();
+    if (title.includes(searchQuery) || genre.includes(searchQuery)) {
+      filteredPods.push(pod);
+    }
+
 
   }
-  appendPods(filteredPods);
+  appendPods(filteredPods, "#suggested");
 }
 
 
@@ -134,30 +133,87 @@ function search(value) {
 // Skrevet af Chalotte
 
 async function fetchPods() {
-    const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&per_page=4";
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed";
 
-    const response = await fetch(url);
-    const data = await response.json();
-    _pods = data;
-    appendPods(_pods);
+  const response = await fetch(url);
+  const data = await response.json();
+  _pods = data;
 
 }
 
 fetchPods();
 
-// show podcast details
-function appendPods(pods) {
-    let html = "";
 
-    for (const pod of pods) {
-        html += /*html*/ `
+
+
+async function fetchSuggested() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=21";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#suggested");
+
+}
+
+fetchSuggested();
+
+async function fetchNew() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=22";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#newPods");
+
+}
+
+fetchNew();
+
+
+async function fetchOther() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=24";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#OtherSuggests");
+
+}
+
+fetchOther();
+
+
+
+async function fetcCategory() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=34";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendCategory(pods, "#category");
+
+}
+
+fetcCategory();
+
+
+
+// show podcast details
+function appendPods(pods, element) {
+  let html = "";
+
+  for (const pod of pods) {
+    html += /*html*/ `
         <article class="index-cards" onclick ="showDetailView(${pod.id})">
         
         <div class="hexagon hexagon2">
             <div class="hexagon-in1">
-                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})">
-                
-                </div>
+                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})"></div>
             </div>
         </div>
         <div class="index-card-text">
@@ -168,48 +224,69 @@ function appendPods(pods) {
         </div>
         </article>
         `;
-    }
-    document.querySelector("#suggested").innerHTML = html;
+  }
+  document.querySelector(element).innerHTML = html;
+}
+
+
+function appendCategory(categories, element) {
+  let html = "";
+
+  for (const category of categories) {
+    html += /*html*/ `
+      <article class="kategori-kasse" onclick="navigateTo('#/recommended')">
+      
+      <div class="hexagon hexagonKategori">
+          <div class="hexagon-in1">
+              <div class="hexagon-in2Kategori" style="background-image: url(${category.acf.img})"></div>
+          </div>
+      </div>
+      <div class="index-card-text">
+      <h4>${category.title.rendered}</h4>
+      <h5>${getCount(category)} emner</h5>
+      </div>
+      </article>
+      `;
+  }
+  document.querySelector(element).innerHTML = html;
+}
+
+function getCount(category) {
+  let count = "";
+  if (`http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=${category.name}` == `http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed=${category.title.rendered}`) {
+    count = category.count;
+  } else {
+    count = "X"
+  }
+  return count;
 }
 
 // Fortæller hvilket billede der skal bruges alt efter rating
 
 function getStars(pod) {
-    let star = "";
-    if (pod.acf.rating === "2") {
-        star = "img/2star.png";
-    } else if (pod.acf.rating === "3") {
-        star = "img/3star.png";
-    } else if (pod.acf.rating === "4") {
-        star = "img/4star.png";
-    } else if (pod.acf.rating === "5") {
-        star = "img/5star.png";
-    } else {
-        star = "img/1star.png";
-    }
-    return star;
-}
-
-// ========================= Categories ======================================
-
-async function fetchCategories() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/categories?_embed";
-
-  const response = await fetch(url);
-  const data = await response.json();
-  appendCategories(data);
-
-}
-
-fetchCategories();
-
-function appendCategories(categories) {
-  let htmlTemplate = "";
-
-  for (let category of categories) {
-      htmlTemplate += /*html*/ `
-      <option value="${category.id}">${category.name}</option>`;
+  let star = "";
+  if (pod.acf.rating === "2") {
+    star = "img/2star.png";
+  } else if (pod.acf.rating === "3") {
+    star = "img/3star.png";
+  } else if (pod.acf.rating === "4") {
+    star = "img/4star.png";
+  } else if (pod.acf.rating === "5") {
+    star = "img/5star.png";
+  } else {
+    star = "img/1star.png";
   }
-  console.log(htmlTemplate);
-  document.querySelector('#select-category').innerHTML += htmlTemplate;
+  return star;
+}
+
+
+//=============================== Emner =======================================
+
+
+
+
+// =============================== Til top function =============================0
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
