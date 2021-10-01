de8917cd83c04f83acc17108a148deacce3ba741
ChaEma7 <70863947+ChaEma7@users.noreply.github.com>
2021-10-01 19:12:46 +0200

    01-10-2021
    Hvis søg ikke virker. kender vi nogenlunde årsagen og arbejder på en løsning

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -118,6 +118,248 @@ async function updateJSONBIN(users) {
   appendUsers(result.record);
 }
 
+
+// ======================= fetch podcasts =============================
+// Skrevet af Chalotte og Line
+
+
+async function ultimateFetch() {
+await fetchPods();
+fetchSuggested();
+fetchAllSuggested();
+fetchNew();
+fetchOther();
+fetchAllCategories();
+fetchCategory();
+}
+
+ultimateFetch()
+
+/* Her har vi samlet alle vores kald af fetch funktioner i en funktion. Det gør vi, fordi vi gerne vil sikre os, at JS engine venter
+med at læse videre i koden til den har modtaget de ønskede data fra fetchPods(). 
+Denne data samles i vores global variabel _pods.*/
+
+async function fetchPods() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed";
+
+  const response = await fetch(url);
+  const data = await response.json();
+  _pods = data;
+
+}
+
+/* Henter alle posts (eller objekter i arrayet) ned fra vores json url. async og await er en syntaks der fortæller, 
+at JS engine skal vente med at læse videre i koden til den har modtaget data fra json. På den måde sikre vi, 
+at alt indhold vi ønsker fremvist bliver fremvist. 
+Denne data samles i vores global variabel _pods, og vi kan derfor tilgå denne data i alle funktioner*/
+
+
+async function fetchSuggested() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=21";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#suggested");
+
+}
+
+/* Her henter vi kun de posts som tilhøre en bestemt kategori. I dette tilfælde har denne kategori ID'et 21 og er vores
+"foreslået til dig" kategori. Vi vil kun have vist de første 4 posts, derfor deler vi arrayet over med data.slice og giver det
+et agument der hedder (0, 4). Herefter append'er (tilføjer) vi disse til sektionen med ID'et #suggested.
+Denne metode er benyette flere steder nedenfor. */
+
+async function fetchAllSuggested() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=21";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data;
+  appendAllSuggested(pods, "#recommended-container");
+
+}
+
+/* i dette tilfælde ønsker vi alle posts (eller objekter i arrayet) vist. Derfor fjerner vi .slice.
+Vi er dog nød til at lave en ny function her. Havde vi kaldt fetchSuggested() ville vi kun få de 4 første posts. */
+
+
+async function fetchNew() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=22";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#newPods");
+
+}
+
+
+
+
+async function fetchOther() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=24";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendPods(pods, "#OtherSuggests");
+
+}
+
+
+
+async function fetchAllCategories() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=34";
+
+  const response = await fetch(url);
+  const data = await response.json();
+  _categories = data;
+  console.log(_categories)
+  appendCategory(_categories, ".allCategories")
+}
+
+
+
+/* Her skaber vi en ny global variabel som henter alle vores kategorier ned. */
+
+
+
+async function fetchCategory() {
+  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=34";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  let pods = data.slice(0, 4);
+  appendCategory(pods, "#category");
+
+}
+
+
+//=============================== Append =======================================
+// Skrevet af Chalotte og Line
+
+// viser podcast details
+function appendPods(pods, element) {
+  let html = "";
+
+  for (const pod of pods) {
+    html += /*html*/ `
+        <article class="index-cards" onclick ="showDetailView(${pod.id})">
+        
+        <div class="hexagon hexagon2">
+            <div class="hexagon-in1">
+                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})"></div>
+            </div>
+        </div>
+        <div class="index-card-text">
+        <h2>${pod.title.rendered}</h2>
+        <h3>${pod.acf.undertitel}</h3>
+        <p class="genre" >${pod.acf.genre}</p>
+        <img class="stars" src="${getStars(pod)}">
+        </div>
+        </article>
+        `;
+  }
+  document.querySelector(element).innerHTML = html;
+}
+
+/* Her definerer vi, hvordan vores inhentet data fra json skal fremvises. Ved at benyttet et loop (for) 
+ fortæller vi JS engine, at den skal gentage denne funktion for alle objekter i arrayet og, hvor i arrayet den
+ skal finde den ønskede data.
+ I dette tilfælde vil vi gerne fremvise et billede, titlen, undertitlen, genre og rating. Alt dette samles i en article,
+ som ved onclick sender brugeren videre til en datalje side udfra objektets ID.
+ Gennem "document.querySelector(element).innerHTML = html" bliver det sendt DOM'en.
+ Det samme gælder for nedestående append funktioner.*/
+
+
+function appendCategory(categories, element) {
+  let html = "";
+
+  for (const category of categories) {
+    html += /*html*/ `
+      <article class="kategori-kasse" onclick="navigateTo('#/recommended')">
+      
+      <div class="hexagon hexagonKategori">
+          <div class="hexagon-in1">
+              <div class="hexagon-in2" style="background-image: url(${category.acf.img})"></div>
+          </div>
+      </div>
+      <div class="index-card-text">
+      <h4>${category.title.rendered}</h4>
+      <h5>${getCount(category)} emner</h5>
+      </div>
+      </article>
+      `;
+  }
+  document.querySelector(element).innerHTML = html;
+}
+
+function getCount(category) {
+  const categoryTitle = category.title.rendered;
+  const result = _pods.filter(pod => pod.acf.genre === categoryTitle);
+
+  return result.length;
+}
+
+function appendAllSuggested(pods, element) {
+  let html = "";
+
+  for (const pod of pods) {
+    html += /*html*/ `
+        <article class="show-card" onclick ="showDetailView(${pod.id})">
+        
+        <div class="hexagon hexagon-show-card">
+            <div class="hexagon-in1">
+                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})"></div>
+            </div>
+        </div>
+        <div class="show-card-text">
+        <div class="showCard-flex-top">
+        <h2>${pod.acf.langtitel}</h2>
+        <button href="#" class="lyt-nu-knap">Lyt nu</button>
+        </div>
+        <h3>${pod.acf.teaser}</h3>
+        <div class="showCard-flex">
+        <img class="stars2" src="${getStars(pod)}">
+        <p class="genre" >${pod.acf.tid} min</p>
+        <p class="genre" >${pod.acf.genre}</p>
+        </div>
+        </div>
+        </article>
+        `;
+  }
+  document.querySelector(element).innerHTML = html;
+}
+
+// Fortæller hvilket billede der skal bruges alt efter rating
+
+function getStars(pod) {
+  let star = "";
+  if (pod.acf.rating === "2") {
+    star = "img/2star.png";
+  } else if (pod.acf.rating === "3") {
+    star = "img/3star.png";
+  } else if (pod.acf.rating === "4") {
+    star = "img/4star.png";
+  } else if (pod.acf.rating === "5") {
+    star = "img/5star.png";
+  } else {
+    star = "img/1star.png";
+  }
+  return star;
+}
+
+/* Vi vil gerne have at objektes rating bliver vist med stjerner. For at automaticerer dette,
+har vi lavet en if funktion, som vi kalder i append. Denne fungerer således, at hvis objektets rating 
+er 2, skal den hente det billede med 2 stjerne osv. Hvis ikke nogle af vores if'er gør sig gældende,
+må vi gå udfra, at objektet har den mindste rating, og skal derfor hente billedet med 1 stjerne. */
+
+
 // ======================= Search =====================================
 // Skrevet af Chalotte og Line
 
@@ -181,241 +423,6 @@ function search2(value) {
 }
 
 /* Se overstående kommentar. Denne gør det samme, men bliver vist på en anden side.*/
-
-// ======================= fetch podcasts =============================
-// Skrevet af Chalotte og Line
-
-async function fetchPods() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed";
-
-  const response = await fetch(url);
-  const data = await response.json();
-  _pods = data;
-
-}
-
-fetchPods();
-
-/* Henter alle posts (eller objekter i arrayet) ned fra vores json url. async og await er en syntaks der fortæller, 
-at JS engine skal vente med at læse videre i koden til den har modtaget data fra json. På den måde sikre vi, 
-at alt indhold vi ønsker fremvist bliver fremvist. 
-Denne data samles i vores global variabel _pods, og vi kan derfor tilgå denne data i alle funktioner*/
-
-
-async function fetchSuggested() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=21";
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  let pods = data.slice(0, 4);
-  appendPods(pods, "#suggested");
-
-}
-
-fetchSuggested()
-
-/* Her henter vi kun de posts som tilhøre en bestemt kategori. I dette tilfælde har denne kategori ID'et 21 og er vores
-"foreslået til dig" kategori. Vi vil kun have vist de første 4 posts, derfor deler vi arrayet over med data.slice og giver det
-et agument der hedder (0, 4). Herefter append'er (tilføjer) vi disse til sektionen med ID'et #suggested.
-Denne metode er benyette flere steder nedenfor. */
-
-async function fetchAllSuggested() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=21";
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  let pods = data;
-  appendAllSuggested(pods, "#recommended-container");
-
-}
-
-fetchAllSuggested();
-
-/* i dette tilfælde ønsker vi alle posts (eller objekter i arrayet) vist. Derfor fjerner vi .slice.
-Vi er dog nød til at lave en ny function her. Havde vi kaldt fetchSuggested() ville vi kun få de 4 første posts. */
-
-
-async function fetchNew() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=22";
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  let pods = data.slice(0, 4);
-  appendPods(pods, "#newPods");
-
-}
-
-fetchNew();
-
-
-async function fetchOther() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=24";
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  let pods = data.slice(0, 4);
-  appendPods(pods, "#OtherSuggests");
-
-}
-
-fetchOther();
-
-async function fetchAllCategories() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=34";
-
-  const response = await fetch(url);
-  const data = await response.json();
-  _categories = data;
-  console.log(_categories)
-  appendCategory(_categories, ".allCategories")
-}
-
-fetchAllCategories();
-
-/* Her skaber vi en ny global variabel som henter alle vores kategorier ned. */
-
-
-
-async function fetchCategory() {
-  const url = "http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=34";
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  let pods = data.slice(0, 4);
-  appendCategory(pods, "#category");
-
-}
-
-fetchCategory();
-
-
-
-//=============================== Append =======================================
-// Skrevet af Chalotte og Line
-
-// viser podcast details
-function appendPods(pods, element) {
-  let html = "";
-
-  for (const pod of pods) {
-    html += /*html*/ `
-        <article class="index-cards" onclick ="showDetailView(${pod.id})">
-        
-        <div class="hexagon hexagon2">
-            <div class="hexagon-in1">
-                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})"></div>
-            </div>
-        </div>
-        <div class="index-card-text">
-        <h2>${pod.title.rendered}</h2>
-        <h3>${pod.acf.undertitel}</h3>
-        <p class="genre" >${pod.acf.genre}</p>
-        <img class="stars" src="${getStars(pod)}">
-        </div>
-        </article>
-        `;
-  }
-  document.querySelector(element).innerHTML = html;
-}
-
-/* Her definerer vi, hvordan vores inhentet data fra json skal fremvises. Ved at benyttet et loop (for) 
- fortæller vi JS engine, at den skal gentage denne funktion for alle objekter i arrayet og, hvor i arrayet den
- skal finde den ønskede data.
- I dette tilfælde vil vi gerne fremvise et billede, titlen, undertitlen, genre og rating. Alt dette samles i en article,
- som ved onclick sender brugeren videre til en datalje side udfra objektets ID.
- Gennem "document.querySelector(element).innerHTML = html" bliver det sendt DOM'en.
- Det samme gælder for nedestående append funktioner.*/
-
-
-function appendCategory(categories, element) {
-  let html = "";
-
-  for (const category of categories) {
-    html += /*html*/ `
-      <article class="kategori-kasse" onclick="navigateTo('#/recommended')">
-      
-      <div class="hexagon hexagonKategori">
-          <div class="hexagon-in1">
-              <div class="hexagon-in2" style="background-image: url(${category.acf.img})"></div>
-          </div>
-      </div>
-      <div class="index-card-text">
-      <h4>${category.title.rendered}</h4>
-      <h5>${getCount(category)} emner</h5>
-      </div>
-      </article>
-      `;
-  }
-  document.querySelector(element).innerHTML = html;
-}
-
-function getCount(category) {
-  let count = "";
-  if (`http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed&categories=${category.name}` == `http://cmedia-design.dk/wordpress/wp-json/wp/v2/posts?_embed=${category.title.rendered}`) {
-    count = category.count;
-  } else {
-    count = "X"
-  }
-  return count;
-}
-
-function appendAllSuggested(pods, element) {
-  let html = "";
-
-  for (const pod of pods) {
-    html += /*html*/ `
-        <article class="show-card" onclick ="showDetailView(${pod.id})">
-        
-        <div class="hexagon hexagon-show-card">
-            <div class="hexagon-in1">
-                <div class="hexagon-in2" style="background-image: url(${pod.acf.img})"></div>
-            </div>
-        </div>
-        <div class="show-card-text">
-        <div class="showCard-flex-top">
-        <h2>${pod.acf.langtitel}</h2>
-        <button href="#" class="lyt-nu-knap">Lyt nu</button>
-        </div>
-        <h3>${pod.acf.teaser}</h3>
-        <div class="showCard-flex">
-        <img class="stars" src="${getStars(pod)}">
-        <p class="genre" >${pod.acf.tid} min</p>
-        <p class="genre" >${pod.acf.genre}</p>
-        </div>
-        </div>
-        </article>
-        `;
-  }
-  document.querySelector(element).innerHTML = html;
-}
-
-// Fortæller hvilket billede der skal bruges alt efter rating
-
-function getStars(pod) {
-  let star = "";
-  if (pod.acf.rating === "2") {
-    star = "img/2star.png";
-  } else if (pod.acf.rating === "3") {
-    star = "img/3star.png";
-  } else if (pod.acf.rating === "4") {
-    star = "img/4star.png";
-  } else if (pod.acf.rating === "5") {
-    star = "img/5star.png";
-  } else {
-    star = "img/1star.png";
-  }
-  return star;
-}
-
-/* Vi vil gerne have at objektes rating bliver vist med stjerner. For at automaticerer dette,
-har vi lavet en if funktion, som vi kalder i append. Denne fungerer således, at hvis objektets rating 
-er 2, skal den hente det billede med 2 stjerne osv. Hvis ikke nogle af vores if'er gør sig gældende,
-må vi gå udfra, at objektet har den mindste rating, og skal derfor hente billedet med 1 stjerne. */
 
 //=============================== Detail View =======================================
 // Skrevet af Chalotte og Line
